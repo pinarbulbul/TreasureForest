443895df46b53b4c8df549be0ba16a5e3eccbab8
Pınar BÜLBÜL <83332334+pinarbulbul@users.noreply.github.com>
2022-01-05 17:28:33 +0300

    Add project files.

---
+++ Assets/CollectTreasures/Scripts/Collect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour
+{
+    public GameObject collectedLigth; 
+    public Text CollectedTreasuresText;
+    public Text EndOfGameText;
+    private int collectedTreasuresNumber = 0;
+    private int totalTreasuresNumber = 15;//30;
+    public PlayerWalk PlayerWalk;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bonus")
+        {
+            Debug.Log("Congratulations - You Collected A Treasure!");
+            Destroy(collision.gameObject);
+            Instantiate(collectedLigth, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+            collectedTreasuresNumber++;
+            CollectedTreasuresText.text = "Collected Treasures: " + collectedTreasuresNumber.ToString() + "/"+ totalTreasuresNumber.ToString();
+            
+        }
+        if (collectedTreasuresNumber >= totalTreasuresNumber)
+        {
+            EndOfGameText.text = "All treasures are collected! :)";
+            PlayerWalk.endOfGame = true;
+        }
+    }
+}
---
+++ Assets/CollectTreasures/Scripts/PlayerWalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public int playerSpeed;
+    public bool endOfGame;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!endOfGame)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+}
